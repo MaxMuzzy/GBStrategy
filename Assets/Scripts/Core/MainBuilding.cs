8754d5abcd607cd070ec32ce2328f299c48d412d
MaxMuzzy <m.a.saltykov@gmail.com>
2022-09-28 18:38:25 +0300

    HW1_Task1
    Task 1 completed.

--- Assets/Scripts/Core/MainBuilding.cs
+++ Assets/Scripts/Core/MainBuilding.cs
@@ -15,14 +15,23 @@ namespace Core
         [SerializeField] private float _maxHealth = 1000;
         [SerializeField] private Sprite _icon;
 
-        private float _health = 1000;
+        private float _health;
 
+        public void Awake()
+        {
+            _health = _maxHealth;
+        }
         public void ProduceUnit()
         {
             Instantiate(_unitPrefab,
                 new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)),
                 Quaternion.identity,
                 _unitsParent);
+        }
+        public void EnableOutline(bool isEnable)
+        {
+            var outline = gameObject.GetComponent<Outline>();
+            outline.enabled = isEnable;
         }
     }
 }
