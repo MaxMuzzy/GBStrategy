c9a5edf60653db4d049a550d7bb53aa9c8d9170e
MaxMuzzy <m.a.saltykov@gmail.com>
2022-10-04 19:08:54 +0300

    HW3_Task 3
    Task 3 completed.

--- Assets/Scripts/UserControlSystem/UI/Model/AttackableValue.cs
+++ Assets/Scripts/UserControlSystem/UI/Model/AttackableValue.cs
@@ -5,15 +5,8 @@ using UnityEngine;
 namespace UserControlSystem
 {
     [CreateAssetMenu(fileName = nameof(AttackableValue), menuName = "Strategy Game/" + nameof(AttackableValue), order = 0)]
-    public class AttackableValue : ScriptableObject
+    public class AttackableValue : ScriptableObjectValueBase<IAttackable>
     {
-        public IAttackable CurrentValue { get; private set; }
-        public Action<IAttackable> OnNewValue;
 
-        public void SetValue(IAttackable value)
-        {
-            CurrentValue = value;
-            OnNewValue?.Invoke(value);
-        }
     }
 }
---
+++ Assets/Scripts/UserControlSystem/UI/Model/ScriptableObjectValueBase.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+namespace UserControlSystem
+{
+    public abstract class ScriptableObjectValueBase<T> : ScriptableObject
+    {
+        public T CurrentValue { get; private set; }
+        public Action<T> OnNewValue;
+        public void SetValue(T value)
+        {
+            CurrentValue = value;
+            OnNewValue?.Invoke(value);
+        }
+    }
+}
